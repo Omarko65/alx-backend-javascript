29f83ee906a69e94498f7e8a298543a23a0fced9
okor mark <okormark@gmail.com>
2023-04-24 16:50:01 +0100

    task2

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -53,3 +53,13 @@ function isDirector(employee: Director | Teacher): employee is Director {
 function executeWork(employee: Director | Teacher): string {
     return isDirector(employee) ? employee.workDirectorTasks() : employee.workTeacherTasks();
 }
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === 'Math') {
+        return 'Teaching Math';
+    } else if (todayClass === 'History') {
+        return 'Teaching History';
+    }
+}
