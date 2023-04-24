b0e1428ba03dcf699a588322472b21f69d1f72a3
okor mark <okormark@gmail.com>
2023-04-24 04:24:32 +0100

    tasks 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,6 +11,31 @@ interface Directors extends Teacher {
     numberOfReports: number;
 }
 
-function printTeacher(firstName: string, lastName: string) {
-    console.log(`${firstName[0]}. ${lastName}`);
+function printTeacher(firstName: string, lastName: string): string {
+    return `${firstName[0]}. ${lastName}`;
+}
+
+interface StudentClass {
+    firstName: string;
+    lastName: string;
+    workOnHomework: () => void;
+    displayName: () => void;
+}
+
+class StudentMethod implements StudentClass {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName:string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
 }
