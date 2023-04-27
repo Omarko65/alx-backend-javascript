53a5fac5db3835cc1fb0f0ccba5df41f332899a7
okor mark <okormark@gmail.com>
2023-04-27 05:17:23 +0100

    task 4

---
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -0,0 +1,22 @@
+/// <reference path="Teacher.ts" />
+/// <reference path="Subject.ts" />
+/// <reference path="Cpp.ts" />
+
+namespace Subjects {
+    export interface TeacherInterface {
+        experienceTeachingReact?: number;
+    }
+
+    export class React  extends Subject {
+        getRequirements(): string {
+            return 'Here is the list of requirements for React';
+        }
+
+        getAvailableTeacher(): string {
+            if (this.teacher.experienceTeachingReact > 0) {
+                return `Available Teacher: ${this.teacher.firstName}`
+            }
+            return 'No available teacher';
+        }
+    }
+}
