723b6dafd544caaa3a5b2581520819022972ccc6
holic65 <okormark@gmail.com>
2023-06-15 21:32:55 +0000

    fixed error 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,4 +1,4 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 if (process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
