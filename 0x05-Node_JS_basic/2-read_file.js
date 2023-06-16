e7a385fba86308939ea8e92e9c3b4eb7a6f7636c
holic65 <okormark@gmail.com>
2023-06-16 00:52:44 +0000

    fixed eslint error

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,35 +1,34 @@
 const fs = require('fs');
 
-
 function countStudents(file) {
   try {
     let cs = 0;
     let swe = 0;
-    const cs_names = [];
-    const swe_names = [];
+    const csNames = [];
+    const sweNames = [];
 
     const database = fs.readFileSync(file, 'utf-8');
 
-    database.split(/\r?\n/).forEach(line => {
-    databaseContent = line.split(',');
+    database.split(/\r?\n/).forEach((line) => {
+      const databaseContent = line.split(',');
       databaseContent.forEach((data) => {
-        if ('CS' === data) {
-          cs = cs + 1;
-          cs_names.push(databaseContent[0]);
-        } else if ('SWE' === data) {
-          swe = swe + 1;
-          swe_names.push(databaseContent[0]);
+        if (data === 'CS') {
+          cs += 1;
+          csNames.push(databaseContent[0]);
+        } else if (data === 'SWE') {
+          swe += 1;
+          sweNames.push(databaseContent[0]);
         } else {
-          swe = swe + 0;
+          swe += 0;
         }
       });
     });
-  
+
     const students = cs + swe;
-  
-    console.log('Number of students: ' + students);
-    console.log(`Number of students in CS: ${cs}. List: ${cs_names.join(', ')}`);
-    console.log(`Number of students in SWE: ${swe}. List: ${swe_names.join(', ')}`);
+
+    console.log(`Number of students: ${students}`);
+    console.log(`Number of students in CS: ${cs}. List: ${csNames.join(', ')}`);
+    console.log(`Number of students in SWE: ${swe}. List: ${sweNames.join(', ')}`);
   } catch (error) {
     throw new Error('Cannot load the database');
   }
